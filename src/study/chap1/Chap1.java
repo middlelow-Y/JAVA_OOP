8f81c0cb34fac630f52c4e92a210072974fb239d
middlelow-Y <dbswhdgk93@gmail.com>
2023-02-10 10:07:59 +0900

    Initial commit

---
+++ src/study/chap1/Chap1.java
@@ -0,0 +1,7 @@
+package study.chap1;
+
+public class Chap1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/study/chap2/Chap2.java
@@ -0,0 +1,4 @@
+package study.chap2;
+
+public class Chap2 {
+}
